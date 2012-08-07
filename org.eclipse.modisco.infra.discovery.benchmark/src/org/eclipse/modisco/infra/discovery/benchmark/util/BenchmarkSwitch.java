c3511cd5234cc112b40ee840acf56fef369640ae
Gregoire Dupe <gdupe@mia-software.com>
2012-08-07 14:59:57 +0000

    Changes to be able to run with Helios..

--- org.eclipse.modisco.infra.discovery.benchmark/src/org/eclipse/modisco/infra/discovery/benchmark/util/BenchmarkSwitch.java
+++ org.eclipse.modisco.infra.discovery.benchmark/src/org/eclipse/modisco/infra/discovery/benchmark/util/BenchmarkSwitch.java
@@ -13,10 +13,10 @@
  */
 package org.eclipse.modisco.infra.discovery.benchmark.util;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
+import java.util.List;
 
-import org.eclipse.emf.ecore.util.Switch;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 
 import org.eclipse.modisco.infra.discovery.benchmark.*;
 
@@ -33,7 +33,7 @@ import org.eclipse.modisco.infra.discovery.benchmark.*;
  * @see org.eclipse.modisco.infra.discovery.benchmark.BenchmarkPackage
  * @generated
  */
-public class BenchmarkSwitch<T> extends Switch<T> {
+public class BenchmarkSwitch<T> {
 	/**
 	 * The cached model package
 	 * <!-- begin-user-doc -->
@@ -55,16 +55,14 @@ public class BenchmarkSwitch<T> extends Switch<T> {
 	}
 
 	/**
-	 * Checks whether this is a switch for the given package.
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @parameter ePackage the package in question.
-	 * @return whether this is a switch for the given package.
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
-	protected boolean isSwitchFor(EPackage ePackage) {
-		return ePackage == modelPackage;
+	public T doSwitch(EObject theEObject) {
+		return doSwitch(theEObject.eClass(), theEObject);
 	}
 
 	/**
@@ -74,7 +72,26 @@ public class BenchmarkSwitch<T> extends Switch<T> {
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
+	protected T doSwitch(EClass theEClass, EObject theEObject) {
+		if (theEClass.eContainer() == modelPackage) {
+			return doSwitch(theEClass.getClassifierID(), theEObject);
+		}
+		else {
+			List<EClass> eSuperTypes = theEClass.getESuperTypes();
+			return
+				eSuperTypes.isEmpty() ?
+					defaultCase(theEObject) :
+					doSwitch(eSuperTypes.get(0), theEObject);
+		}
+	}
+
+	/**
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
+	 * @generated
+	 */
 	protected T doSwitch(int classifierID, EObject theEObject) {
 		switch (classifierID) {
 			case BenchmarkPackage.BENCHMARK: {
@@ -307,7 +324,6 @@ public class BenchmarkSwitch<T> extends Switch<T> {
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
 	 */
-	@Override
 	public T defaultCase(EObject object) {
 		return null;
 	}
