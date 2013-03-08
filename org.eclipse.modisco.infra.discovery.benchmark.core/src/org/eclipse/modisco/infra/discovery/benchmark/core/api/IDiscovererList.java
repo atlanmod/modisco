ed94ec8265c24253fb8c6a6ef178f47373f6f565
Gregoire Dupe <gdupe@mia-software.com>
2013-03-08 17:38:56 +0000

    Bug 369945 - [Scalability] New generic discoverer benchmark API cleaning

--- org.eclipse.modisco.infra.discovery.benchmark.core/src/org/eclipse/modisco/infra/discovery/benchmark/core/api/IDiscovererList.java
+++ org.eclipse.modisco.infra.discovery.benchmark.core/src/org/eclipse/modisco/infra/discovery/benchmark/core/api/IDiscovererList.java
@@ -27,142 +27,138 @@ public interface IDiscovererList extends List<Discovery> {
 	 * Return the list of Discovery elements
 	 * @return the list
 	 */
-	public abstract List<Discovery> getDiscoverers();
+	public List<Discovery> getDiscoverers();
 
 	/**
 	 * @see java.util.List#add(int, Object)
 	 */
-	public abstract void add(int index,
-			Discovery element);
+	public void add(int index, Discovery element);
 
 	/**
 	 * @see java.util.List#add(Object)
 	 */
-	public abstract boolean add(Discovery e);
+	public boolean add(Discovery e);
 
 	/**
 	 * @see java.util.List#addAll(Collection)
 	 */
-	public abstract boolean addAll(
-			Collection<? extends Discovery> c);
+	public boolean addAll(Collection<? extends Discovery> c);
 
 	/**
 	 * @see java.util.List#addAll(int, Collection)
 	 */
-	public abstract boolean addAll(int index,
-			Collection<? extends Discovery> c);
+	public boolean addAll(int index, Collection<? extends Discovery> c);
 
 	/**
 	 * @see java.util.List#clear()
 	 */
-	public abstract void clear();
+	public void clear();
 
 	/**
 	 * @see java.util.List#contains(Object)
 	 */
-	public abstract boolean contains(Object o);
+	public boolean contains(Object o);
 
 	/**
 	 * @see java.util.List#containsAll(Collection)
 	 */
-	public abstract boolean containsAll(Collection<?> c);
+	public boolean containsAll(Collection<?> c);
 
 	/**
 	 * @see java.util.List#equals(Object)
 	 */
-	public abstract boolean equals(Object o);
+	public boolean equals(Object o);
 
 	/**
 	 * @see java.util.List#get(int)
 	 */
-	public abstract Discovery get(int index);
+	public Discovery get(int index);
 
 	/**
 	 * @see java.util.List#hashCode()
 	 */
-	public abstract int hashCode();
+	public int hashCode();
 
 	/**
 	 * @see java.util.List#indexOf(Object)
 	 */
-	public abstract int indexOf(Object o);
+	public int indexOf(Object o);
 
 	/**
 	 * @see java.util.List#isEmpty()
 	 */
-	public abstract boolean isEmpty();
+	public boolean isEmpty();
 
 	/**
 	 * @see java.util.List#iterator()
 	 */
-	public abstract Iterator<Discovery> iterator();
+	public Iterator<Discovery> iterator();
 
 	/**
 	 * @see java.util.List#lastIndexOf(Object)
 	 */
-	public abstract int lastIndexOf(Object o);
+	public int lastIndexOf(Object o);
 
 	/**
 	 * @see java.util.List#listIterator()
 	 */
-	public abstract ListIterator<Discovery> listIterator();
+	public ListIterator<Discovery> listIterator();
 
 	/**
 	 * @see java.util.List#listIterator(int)
 	 */
-	public abstract ListIterator<Discovery> listIterator(
+	public ListIterator<Discovery> listIterator(
 			int index);
 
 	/**
 	 * @see java.util.List#remove(int)
 	 */
-	public abstract Discovery remove(int index);
+	public Discovery remove(int index);
 
 	/**
 	 * @see java.util.List#remove(Object)
 	 */
-	public abstract boolean remove(Object o);
+	public boolean remove(Object o);
 
 	/**
 	 * @see java.util.List#removeAll(Collection)
 	 */
-	public abstract boolean removeAll(Collection<?> c);
+	public boolean removeAll(Collection<?> c);
 
 	/**
 	 * @see java.util.List#retainAll(Collection)
 	 */
-	public abstract boolean retainAll(Collection<?> c);
+	public boolean retainAll(Collection<?> c);
 
 	/**
 	 * @see java.util.List#set(int, Object)
 	 */
-	public abstract Discovery set(int index,
+	public Discovery set(int index,
 			Discovery element);
 
 	/**
 	 * @see java.util.List#size()
 	 */
-	public abstract int size();
+	public int size();
 
 	/**
 	 * @see java.util.List#subList(int, int)
 	 */
-	public abstract List<Discovery> subList(
-			int fromIndex, int toIndex);
+	public List<Discovery> subList(int fromIndex, int toIndex);
 
 	/**
 	 * @see java.util.List#toArray()
 	 */
-	public abstract Object[] toArray();
+	public Object[] toArray();
 
 	/**
 	 * @see java.util.List#toArray(Object[])
 	 */
-	public abstract <T> T[] toArray(T[] a);
+	public <T> T[] toArray(T[] a);
 	
 	/**
 	 * @see java.util.List#toString()
 	 */
-	public abstract String toString();
+	public String toString();
 
 }
