684be5e6707f6a26d08e9d03755ed191613d77bb
Thomas Cicognani <tcicognani@sodifrance.fr>
2014-08-26 11:15:10 +0200

    Bug 470903 - The custom HideDerivedReferences doesn't use the super method
    Change-Id: Ieecbe0cca8ac5ee551a1c13efd363ee3ed2053a0
    Signed-off-by: Thomas Cicognani <tcicognani@sodifrance.fr>

--- org.eclipse.modisco.infra.browser.ecore.core/src/org/eclipse/modisco/infra/browser/ecore/core/internal/hidederviedreferences/IsVisibleQuery.java
+++ org.eclipse.modisco.infra.browser.ecore.core/src/org/eclipse/modisco/infra/browser/ecore/core/internal/hidederviedreferences/IsVisibleQuery.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2014 Mia-Software.
+ * Copyright (c) 2014, 2015 Mia-Software, and Soft-Maint.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *    Gregoire Dupe (Mia-Software) - Bug 358914 - [Move to EMF Facet][Browser] Switch to EMF Facet
+ *    Thomas Cicognani (Soft-Maint) - Bug 470903 - The custom HideDerivedReferences doesn't use the super method
  */
 package org.eclipse.modisco.infra.browser.ecore.core.internal.hidederviedreferences;
 
@@ -14,27 +15,48 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.facet.efacet.core.IFacetManager;
 import org.eclipse.emf.facet.efacet.core.exception.DerivedTypedElementException;
-import org.eclipse.emf.facet.query.java.core.IJavaQuery2;
+import org.eclipse.emf.facet.efacet.core.exception.FacetManagerException;
+import org.eclipse.emf.facet.efacet.core.exception.SuperInvokeException;
+import org.eclipse.emf.facet.efacet.metamodel.v0_2_0.efacet.DerivedTypedElement;
+import org.eclipse.emf.facet.query.java.core.IJavaQuery3;
 import org.eclipse.emf.facet.query.java.core.IParameterValueList2;
 
-public class IsVisibleQuery implements IJavaQuery2<EObject, Boolean> {
-	public Boolean evaluate(final EObject context, 
+public class IsVisibleQuery implements IJavaQuery3<EObject, Boolean> {
+	public Boolean evaluate(final EObject source,
 			final IParameterValueList2 parameterValues,
-			final IFacetManager facetManager)
+			final IFacetManager facetManager, final DerivedTypedElement feature)
 			throws DerivedTypedElementException {
 		boolean result = false;
 		if (parameterValues.size() == 0) {
+			throw new DerivedTypedElementException(
+					"parameterValues must not be empty"); //$NON-NLS-1$
+		}
+
+		final Object paramValue = parameterValues.get(0).getValue();
+		if (paramValue instanceof EStructuralFeature) {
+			final EStructuralFeature eStructFeature = (EStructuralFeature) paramValue;
+			result = !eStructFeature.isDerived();
+		} else if (paramValue == null) {
 			result = true;
 		} else {
-			final Object paramValue = parameterValues.get(0).getValue();
-			if (paramValue instanceof EStructuralFeature) {
-				final EStructuralFeature eStructFeature = (EStructuralFeature) paramValue;
-				result = !eStructFeature.isDerived();
-			} else {
-				throw new DerivedTypedElementException(
-						"Unexpected parameter type"); //$NON-NLS-1$
-			}
+			final String message = String
+					.format("Unexpected parameter type (%s instead of EStructuralFeature)", //$NON-NLS-1$
+							paramValue.getClass().getName());
+			throw new DerivedTypedElementException(message);
 		}
-		return  Boolean.valueOf(result);
+
+		try {
+			/*
+			 * The object is visible if this custom says it is AND if the
+			 * previous/super customs say it is
+			 */
+			result = result
+					&& facetManager.getSuperValueOf(feature, source,
+							parameterValues, Boolean.class).booleanValue();
+		} catch (Exception e) {
+			throw new DerivedTypedElementException(e);
+		}
+
+		return Boolean.valueOf(result);
 	}
 }
