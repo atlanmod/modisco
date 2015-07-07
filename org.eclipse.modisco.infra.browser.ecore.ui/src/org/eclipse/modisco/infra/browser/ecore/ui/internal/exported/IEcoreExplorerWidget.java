6820f5d36d6c94826566c016a5eeda9c199c7b63
Grégoire Dupé <gdupe@mia-software.com>
2015-07-07 12:33:57 +0200

    Bug 471020 - Ecore Explorer View
    Adding @noextend and @noimplement tags.
    
    Change-Id: I7b3e4619ed475ce9b9946a9fdcb24e50a4becd03

--- org.eclipse.modisco.infra.browser.ecore.ui/src/org/eclipse/modisco/infra/browser/ecore/ui/internal/exported/IEcoreExplorerWidget.java
+++ org.eclipse.modisco.infra.browser.ecore.ui/src/org/eclipse/modisco/infra/browser/ecore/ui/internal/exported/IEcoreExplorerWidget.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2015 Soft-Maint
+ * Copyright (c) 2015 Soft-Maint, and Mia-Software
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *    Thomas Cicognani (Soft-Maint) - Bug 471020 - Ecore Explorer View
  *    Thomas Cicognani (Soft-Maint) - Bug 472041 - [New Browser] Add a customization counting instances by EClass in the same Resource
+ *    Grégoire Dupé (Mia-Software) - Bug 471020 - Ecore Explorer View
  */
 package org.eclipse.modisco.infra.browser.ecore.ui.internal.exported;
 
@@ -18,6 +19,10 @@ import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
+/**
+ * @noextend
+ * @noimplement
+ */
 public interface IEcoreExplorerWidget extends IAdaptable {
 
 	void changeInput(Collection<EObject> eObjects);
--- org.eclipse.modisco.infra.browser.ecore.ui/src/org/eclipse/modisco/infra/browser/ecore/ui/internal/exported/IView.java
+++ org.eclipse.modisco.infra.browser.ecore.ui/src/org/eclipse/modisco/infra/browser/ecore/ui/internal/exported/IView.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2015 Soft-Maint
+ * Copyright (c) 2015 Soft-Maint, and Mia-Software
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,9 +7,16 @@
  * 
  * Contributors:
  *    Thomas Cicognani (Soft-Maint) - Bug 471020 - Ecore Explorer View
+ *    Grégoire Dupé (Mia-Software) - Bug 471020 - Ecore Explorer View
  */
 package org.eclipse.modisco.infra.browser.ecore.ui.internal.exported;
 
+//TODO Must be moved to org.eclipe.modisco.util.ui
+
+/**
+ * @noextend
+ * @noimplement
+ */
 public interface IView<W extends Object> {
 
 	W getWidget();
