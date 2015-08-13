3ee6abc40c821cfa1eb251fc96e06440504a90e5
Grégoire Dupé <gdupe@mia-software.com>
2015-08-13 14:46:49 +0200

    Bug 474893 - [Deprecated] org.eclipse.modisco.jee.webapp.discoverer.actions.WebXmlDiscoverer

--- org.eclipse.modisco.jee.webapp.discoverer.tests/src/org/eclipse/modisco/jee/webapp/discoverer/tests/compatibility/TestWebXml22.java
+++ org.eclipse.modisco.jee.webapp.discoverer.tests/src/org/eclipse/modisco/jee/webapp/discoverer/tests/compatibility/TestWebXml22.java
@@ -42,8 +42,9 @@ import org.junit.Test;
 
 /**
  * Test of discovering a WEB.XML model 2.2
- * 
+ * @deprecated cf. https://bugs.eclipse.org/bugs/show_bug.cgi?id=474893
  */
+@Deprecated
 public class TestWebXml22 {
 
 	private static final String RESOURCES_TEST_XML = "/resources/TestWebXml22.xml"; //$NON-NLS-1$
--- org.eclipse.modisco.jee.webapp.discoverer.tests/src/org/eclipse/modisco/jee/webapp/discoverer/tests/compatibility/TestWebXml30.java
+++ org.eclipse.modisco.jee.webapp.discoverer.tests/src/org/eclipse/modisco/jee/webapp/discoverer/tests/compatibility/TestWebXml30.java
@@ -42,8 +42,9 @@ import org.junit.Test;
 
 /**
  * Test of discovering a WEB.XML model 3.0
- * 
+ * @deprecated cf. https://bugs.eclipse.org/bugs/show_bug.cgi?id=474893
  */
+@Deprecated
 public class TestWebXml30 {
 
 	private static final String RESOURCES_TEST1_XML = "/resources/TestWebXml30.xml"; //$NON-NLS-1$
