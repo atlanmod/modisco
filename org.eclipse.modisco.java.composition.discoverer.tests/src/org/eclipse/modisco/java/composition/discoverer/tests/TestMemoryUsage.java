4f8ad95a184431e95b9d43106a731b73fa4f08a0
Grégoire Dupé <gdupe@mia-software.com>
2015-10-08 16:20:15 +0200

    Bug 479339 - [Unit Test Failure] org.eclipse.modisco.java.composition.discoverer.tests.TestMemoryUsage.test002
    Increasing threshold to 180Mo.
    + warning fixing
    
    Change-Id: I955c4ce04beeed87d20de419ac9d210beb643b25

--- org.eclipse.modisco.java.composition.discoverer.tests/src/org/eclipse/modisco/java/composition/discoverer/tests/TestMemoryUsage.java
+++ org.eclipse.modisco.java.composition.discoverer.tests/src/org/eclipse/modisco/java/composition/discoverer/tests/TestMemoryUsage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Mia-Software.
+ * Copyright (c) 2011, 2015 Mia-Software.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *    Nicolas Guyomar (Mia-Software) - initial API and implementation
  *    Nicolas Bros (Mia-Software) - Bug 335003 - [Discoverer] : Existing Discoverers Refactoring based on new framework
+ *    Grégoire Dupé (Mia-Software) - Bug 479339 - [Unit Test Failure] org.eclipse.modisco.java.composition.discoverer.tests.TestMemoryUsage.test002
  *******************************************************************************/
 package org.eclipse.modisco.java.composition.discoverer.tests;
 
@@ -15,8 +16,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.Iterator;
-
-import junit.framework.Assert;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -29,50 +28,49 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.gmt.modisco.infra.common.core.logging.MoDiscoLogger;
-import org.eclipse.modisco.java.discoverer.tests.Activator;
+import org.eclipse.emf.facet.util.core.Logger;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.modisco.infra.discovery.core.exception.DiscoveryException;
 import org.eclipse.modisco.java.composition.discoverer.DiscoverKDMSourceAndJavaModelFromJavaProject;
 import org.eclipse.modisco.java.composition.javaapplication.JavaApplication;
+import org.eclipse.modisco.java.discoverer.tests.Activator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 
+import junit.framework.Assert;
+
 public class TestMemoryUsage {
 
+	private static final long MAX_MEM_AWAITED = 180;
+	private static final int KILO = 1024;
 	private static final String PROJECT_NAME = JUnitPlugin.PLUGIN_ID
 			+ "_test001"; //$NON-NLS-1$
 
 	private static Resource resource = null;
 	private static JavaApplication model;
 
-	private static final long AWAITED_MEM_MAXIMAL = 100;
-
 	private long memoryUsed;
 	private IProject project = null;
 
-	private final int int1024 = 1024;
-
 	@Before
-	public void initResource() throws Exception {
-
+	public void initResource() throws CoreException, IOException,
+			InterruptedException, DiscoveryException {
 		final long totalMemory = Runtime.getRuntime().totalMemory();
-
 		this.project = ResourcesPlugin.getWorkspace().getRoot()
 				.getProject(TestMemoryUsage.PROJECT_NAME);
-
 		if (this.project.exists()) {
 			this.project.delete(true, true, new NullProgressMonitor());
 		}
 		this.project.create(new NullProgressMonitor());
 		this.project.open(new NullProgressMonitor());
 		deepCopy("/workspace/test001/", this.project, "/"); //$NON-NLS-1$ //$NON-NLS-2$
-		IJavaProject javaProject = JavaCore.create(this.project);
+		final IJavaProject javaProject = JavaCore.create(this.project);
 		addSystemLibraries(javaProject);
 		this.project.refreshLocal(IResource.DEPTH_INFINITE,
 				new NullProgressMonitor());
@@ -81,17 +79,16 @@ public class TestMemoryUsage {
 		Job.getJobManager().join(ResourcesPlugin.FAMILY_MANUAL_REFRESH, null);
 		Job.getJobManager().join(ResourcesPlugin.FAMILY_MANUAL_BUILD, null);
 		discoverResource(javaProject);
-		EList<EObject> i = TestMemoryUsage.getResource().getContents();
-		for (Iterator<EObject> iterator = i.iterator(); iterator.hasNext()
+		final EList<EObject> roots = TestMemoryUsage.getResource().getContents();
+		for (final Iterator<EObject> iterator = roots.iterator(); iterator.hasNext()
 				&& TestMemoryUsage.model == null;) {
-			EObject eobject = iterator.next();
+			final EObject eobject = iterator.next();
 			if (eobject instanceof JavaApplication) {
 				TestMemoryUsage.model = (JavaApplication) eobject;
 			}
 		}
-
-		this.memoryUsed = (Runtime.getRuntime().totalMemory() - totalMemory)
-				/ this.int1024 / this.int1024;
+		final long deltaMem = Runtime.getRuntime().totalMemory() - totalMemory;
+		this.memoryUsed = deltaMem / KILO / KILO;
 	}
 
 	@After
@@ -103,56 +100,60 @@ public class TestMemoryUsage {
 
 	@Test
 	public void test002() {
+		final String message = String.format(
+				"Abnormal memory use for Java Composition Discoverer: %s > %s", //$NON-NLS-1$
+				Long.toString(this.memoryUsed),
+				Long.toString(TestMemoryUsage.MAX_MEM_AWAITED));
 		Assert.assertTrue(
-				"Abnormal memory use for Java Composition Discoverer " + "\n " + this.memoryUsed //$NON-NLS-1$ //$NON-NLS-2$
-						+ " > " + TestMemoryUsage.AWAITED_MEM_MAXIMAL, //$NON-NLS-1$
-				this.memoryUsed < TestMemoryUsage.AWAITED_MEM_MAXIMAL);
+				message, 
+				this.memoryUsed < TestMemoryUsage.MAX_MEM_AWAITED);
 	}
 
 	private static void discoverResource(final IJavaProject javaProject)
-			throws Exception {
-		DiscoverKDMSourceAndJavaModelFromJavaProject discoverer = new DiscoverKDMSourceAndJavaModelFromJavaProject();
+			throws DiscoveryException {
+		final DiscoverKDMSourceAndJavaModelFromJavaProject discoverer =
+				new DiscoverKDMSourceAndJavaModelFromJavaProject();
 		discoverer.discoverElement(javaProject, new NullProgressMonitor());
 		TestMemoryUsage.resource = discoverer.getTargetModel();
 	}
 
 	private void deepCopy(final String path, final IProject projectTarget,
-			final String destinationPath) throws CoreException, IOException {
-		Bundle bundle = Activator.getDefault().getBundle();
-		Enumeration<?> e = Activator.getDefault().getBundle()
+			final String destPath) throws CoreException, IOException {
+		final Bundle bundle = Activator.getDefault().getBundle();
+		final Enumeration<?> entityPath = Activator.getDefault().getBundle()
 				.getEntryPaths(path);
-		if (e == null) {
+		if (entityPath == null) {
 			try {
 				// destinationPath);
 				InputStream source = bundle.getEntry(path).openStream();
-				IFile javaFile = projectTarget.getFile(destinationPath);
+				final IFile javaFile = projectTarget.getFile(destPath);
 				if (javaFile.exists()) {
 					javaFile.delete(true, new NullProgressMonitor());
 				}
 				javaFile.create(source, true, new NullProgressMonitor());
 				// + destinationPath);
 			} catch (Exception e1) {
-				MoDiscoLogger.logError(e1, JUnitPlugin.getDefault());
+				Logger.logError(e1, JUnitPlugin.getDefault());
 			}
 		} else {
-			String subDestinationPath = "/"; //$NON-NLS-1$
-			if (!destinationPath.equals("/")) { //$NON-NLS-1$
-				IFolder folder = projectTarget.getFolder(destinationPath);
+			String subDestPath = "/"; //$NON-NLS-1$
+			if (!"/".equals(destPath)) { //$NON-NLS-1$
+				final IFolder folder = projectTarget.getFolder(destPath);
 				if (!folder.exists()) {
 					try {
 						folder.create(true, true, new NullProgressMonitor());
 					} catch (Exception e1) {
-						MoDiscoLogger.logError(e1, JUnitPlugin.getDefault());
+						Logger.logError(e1, JUnitPlugin.getDefault());
 					}
 				}
-				subDestinationPath = folder.getProjectRelativePath().toString();
+				subDestPath = folder.getProjectRelativePath().toString();
 			}
-			while (e.hasMoreElements()) {
-				Object object = e.nextElement();
+			while (entityPath.hasMoreElements()) {
+				final Object object = entityPath.nextElement();
 				if (object instanceof String) {
-					String subpath = (String) object;
+					final String subpath = (String) object;
 					if (!subpath.matches(".*/\\.svn/")) { //$NON-NLS-1$
-						String dest = subDestinationPath
+						final String dest = subDestPath
 								+ subpath.substring(path.length() - 1);
 						deepCopy(subpath, projectTarget, dest);
 					}
@@ -165,8 +166,8 @@ public class TestMemoryUsage {
 
 	private static void addSystemLibraries(final IJavaProject javaProject)
 			throws JavaModelException {
-		IClasspathEntry[] oldEntries = javaProject.getRawClasspath();
-		IClasspathEntry[] newEntries = new IClasspathEntry[oldEntries.length + 1];
+		final IClasspathEntry[] oldEntries = javaProject.getRawClasspath();
+		final IClasspathEntry[] newEntries = new IClasspathEntry[oldEntries.length + 1];
 		System.arraycopy(oldEntries, 0, newEntries, 0, oldEntries.length);
 		newEntries[oldEntries.length] = JavaRuntime
 				.getDefaultJREContainerEntry();
