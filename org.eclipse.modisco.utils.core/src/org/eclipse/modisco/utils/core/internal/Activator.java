b8dcf4a5cb585e6ac3bc3ca8031b6da69566bac9
Grégoire Dupé <gdupe@mia-software.com>
2015-11-20 18:53:00 +0100

    Bug 482715 - NumberFormatException in SystemInfo.computeInfo (180)

--- org.eclipse.modisco.utils.core/src/org/eclipse/modisco/utils/core/internal/Activator.java
+++ org.eclipse.modisco.utils.core/src/org/eclipse/modisco/utils/core/internal/Activator.java
@@ -1,44 +1,51 @@
 /*******************************************************************************
- * Copyright (c) 2012 INRIA. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
+ * Copyright (c) 2012, 2015 INRIA and Mia-Software.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 which 
+ * accompanies this distribution, and is available at 
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: Guillaume Doux - INRIA - Initial API and implementation
- * 
+ * Contributors:
+ *    Guillaume Doux - INRIA - Initial API and implementation
+ *    Grégoire Dupé (Mia-Software) - Bug 482715 - NumberFormatException in SystemInfo.computeInfo (180)
  ******************************************************************************/
 package org.eclipse.modisco.utils.core.internal;
 
-import org.osgi.framework.BundleActivator;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * 
- * @author Guillaume Doux
- *
- */
-public class Activator implements BundleActivator {
+@SuppressWarnings("PMD.UseSingleton")
+// @SuppressWarnings("PMD.UseSingleton") We cannot conforms to this rule because
+// this class is an Eclipse plug-in activator which will be instantiated by the
+// Eclipse framework.
+public class Activator extends Plugin {
 
-	private static BundleContext context;
+	private static Activator plugin;
 
-	static BundleContext getContext() {
-		return context;
+	@Override
+	@SuppressWarnings("PMD.SignatureDeclareThrowsException")
+	// @SuppressWarnings("PMD.SignatureDeclareThrowsException") We have to use
+	// this signature because this is an override of an Eclipse framework's
+	// method.
+	public void start(final BundleContext bundleContext) throws Exception {
+		super.start(bundleContext);
+		Activator.plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+	@Override
+	@SuppressWarnings("PMD.SignatureDeclareThrowsException")
+	// @SuppressWarnings("PMD.SignatureDeclareThrowsException") We have to use
+	// this signature because this is an override of an Eclipse framework's
+	// method.
+	public void stop(final BundleContext bundleContext) throws Exception {
+		// NOPMD: PMD say "Assigning an Object to null is a code smell."
+		// No choice to right it in another way : this is an Eclipse pattern.
+		Activator.plugin = null; // NOPMD by gdupe on 30/03/12 10:19
+		super.stop(bundleContext);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+	public static Plugin getDefault() {
+		return Activator.plugin;
 	}
 
 }
